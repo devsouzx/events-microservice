925e52c6be0f153a5a502df8d8f5e2479150166a
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-10-15 21:54:03 -0300

    implementing spring cloud

--- src/main/java/com/devsouzx/events_microservice/services/EmailServiceClient.java
+++ src/main/java/com/devsouzx/events_microservice/services/EmailServiceClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "email-service", url = "http://localhost:8090/api/email")
+@FeignClient(value = "EMAIL-SERVICE")
 public interface EmailServiceClient {
-    @PostMapping
+    @PostMapping("/api/email")
     void sendEmail(@RequestBody EmailRequest emailRequest);
 }
